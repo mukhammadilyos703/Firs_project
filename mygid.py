60fd948f11be5f1cc6b1f649944d46bcc1e194fe
Muhammadilyos <mukhammadilyos777@gmail.com>
2023-12-19 15:10:53 +0500

    i changed

---
+++ mygid.py
@@ -0,0 +1,8 @@
+class Talaba:
+    def __init__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+    
+
+t1 = Talaba("ali", "Aliev", 18)
